a64149985d9b05fe91f81a72ea11fbf7c85a61a6
Aarthi R <166208418+aarthyy25@users.noreply.github.com>
2024-08-30 10:36:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-// import Header from './components/header';
-// import Navbar from './components/nav';
 import Body from './components/body';
 function App(){
   return(
-    <div class="std">
-      {/* <Navbar /> */}
-      {/* <Header /> */}
+    <div>
       <Body />
     </div>
   )
