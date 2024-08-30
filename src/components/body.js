ca56de015f0aefc768b3a3df2291c8d5e31470d4
Aarthi R <166208418+aarthyy25@users.noreply.github.com>
2024-08-30 10:38:13 +0530

    Update body.js

--- src/components/body.js
+++ src/components/body.js
@@ -1,97 +1,115 @@
-import React,{useState,useEffect} from 'react';
+import React,{useState,useEffect,useRef} from 'react';
+import logo from './assests/logo.jpeg';
+import { FaUsers } from "react-icons/fa";
+import { FaUserGraduate } from "react-icons/fa";
+import { PiFilesLight } from "react-icons/pi";
+import { AiFillQuestionCircle } from "react-icons/ai";
+import { MdOutlineSettings } from "react-icons/md";
+import { TbLogout } from "react-icons/tb";
 import { FaUserAlt } from "react-icons/fa";
 import { FaBook } from "react-icons/fa6";
 import { MdOutlineNaturePeople } from "react-icons/md";
 import { FaBookReader } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { BsFileEarmarkBarGraphFill } from "react-icons/bs";
-import { LuBuilding } from "react-icons/lu";
-
-
 
 function Body(){
-    
-        let[count1,setCount1]=useState(0);
-        let[count2,setCount2]=useState(0);
-        let[count3,setCount3]=useState(0);
-    function Count(){
-        useEffect(()=>{
-            setTimeout(()=>{
-                setCount1(count1=>count1+1)
-                setCount2(count2=>count2+1)
-                setCount3(count3=>count3+1)
-             },2000)
+    let[count1,setCount1]=useState(0);
+    let[count2,setCount2]=useState(0);
+    let[count3,setCount3]=useState(0);
+    count1=useRef(0);
+    count2=useRef(0);
+    count3=useRef(0);
+    useEffect(()=>{
+        setTimeout(()=>{
+            count1.current=count1.current+1;
         })
-    }
+    })
+    useEffect(()=>{
+        setTimeout(()=>{
+            count2.current=count2.current+1;
+        })
+    })
+    useEffect(()=>{
+        setTimeout(()=>{
+            count3.current=count3.current+1;
+        })
+    })
     return(
         <div>
             <section>
                 <div class="container">
-                    <div class="row boxes">
-                        <div class='col-2'></div>
-                        <div class="col-3 card">
+                    <div class="row">
+                        <div class="col-3 nav">
                             <div>
-                                <span><FaUserAlt /></span>
-                                <span>{count1}</span>
-                                <div>Regular Students</div>
+                                <img src={logo} alt="Logo image"></img>
+                            </div>
+                            <a><FaUsers /></a>
+                            <p>Student Management</p>
+                            <a><FaUserGraduate /></a>
+                            <p>Alumini Association</p>
+                            <a><PiFilesLight /></a>
+                            <p>Attendance</p>
+                            <div class="foot">
+                                <a><AiFillQuestionCircle /></a>
+                                <a><MdOutlineSettings /></a>
+                                <a><TbLogout /></a>
                             </div>
                         </div>
-                        <div class="col-3 card">
-                            <div>
-                                <span><FaBook /></span>
-                                <span>{count2}</span>
-                                <div>Remedial Students</div>
+                        <div class="col-9 content">
+                            <h1>Student Management System</h1>
+                            <div class="cards">
+                                <div class="card1">
+                                    <span class="icon"><a><FaUserAlt /></a></span>
+                                    <span>{count1.current}</span>
+                                    <div><p>Regular Students</p></div>
+                                </div>
+                                <div class="card2">
+                                    <span class="icon"><a><FaBook /></a></span>
+                                    <span>{count2.current}</span>
+                                    <div><p>Remedial Students</p></div>
+                                </div>
+                                <div class="card3">
+                                    <span class="icon"><a><MdOutlineNaturePeople /></a></span>
+                                    <span>{count3.current}</span>
+                                    <div><p>In paid clubs</p></div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-3 card">
-                            <div>
-                                <span><MdOutlineNaturePeople /></span>
-                                <span>{count3}</span>
-                                <div>In paid clubs</div>
+                            <h2>Menu</h2>
+                            <div class="row1">
+                                <button onclick={()=>{
+                                    setCount1(prev1=>prev1+1)
+                                }}>
+                                    <div><a><FaUserAlt /></a></div>
+                                    <div>Regular Employement</div>
+                                </button>
+                                <button onClick={()=>{
+                                    setCount2(prev2=>prev2+1)
+                                }}>
+                                    <div><a><FaBook /></a></div>
+                                    <div>Remedial Employement</div>
+                                </button>
+                                <button onclick={()=>{
+                                    setCount3(prev3=>prev3+1)
+                                }}>
+                                    <div><a><MdOutlineNaturePeople /></a></div>
+                                    <div>Club Management</div>
+                                </button>
                             </div>
-                        </div>
-                    </div>
-                    <div class="row menu">
-                        <div class="col-2"></div>
-                        <div class="col-10">
-                            <h4>Menu</h4> 
-                        </div>
-                    </div>
-                    <div class="row menu">
-                        <div class="col-2"></div>
-                        <div class="col-3">
-                            <div><FaUserAlt /></div>
-                            <button onclick={Count()}>Regular Employement</button>
-                        </div>
-                        <div class="col-3">
-                            <div><FaBook /></div>
-                            <button onClick={Count()}>Remedial Employement</button>
-                        </div>
-                        <div class="col-3">
-                            <div><MdOutlineNaturePeople /></div>
-                            <button onClick={Count()}>Club Management</button>
-                        </div>
-                    </div>
-                    <div class="row menu">
-                        <div class="col-2"></div>
-                        <div class="col-3">
-                            <div><FaBookReader /></div>
-                            <div>Classroom Management</div>
-                        </div>
-                        <div class="col-3">
-                            <div><MdEmail /></div>
-                            <div>SMS/Email</div>
-                        </div>
-                        <div class="col-3">
-                            <div><BsFileEarmarkBarGraphFill /></div>
-                            <div>Attendance</div>
-                        </div>
-                    </div>
-                    <div class="row menu">
-                        <div class="col-5"></div>
-                        <div class="col-3">
-                            <div><LuBuilding /></div>
-                            <div>Clinic</div>
+                            <div class="row2">
+                                <button>
+                                    <a><FaBookReader /></a>
+                                    <p>Classroom Management</p>
+                                </button>
+                                <button>
+                                    <a><MdEmail /></a>
+                                    <p>SMS/Email</p>
+                                </button>
+                                <button>
+                                    <a><BsFileEarmarkBarGraphFill /></a>
+                                    <p>Attendance</p>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
